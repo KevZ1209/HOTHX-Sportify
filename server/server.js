fc1d5f086d4ff944ddd35ec26dfc4554e8431f6a
KevZ1209 <kevinbowditch11@gmail.com>
2023-03-05 13:26:07 -0800

    add create-user, check-password, and get-all-users functions

--- server/server.js
+++ server/server.js
@@ -46,6 +46,60 @@ app.get("/get-all-users", async function(req, res) {
     }
 });
 
+app.post("/create-user/:username/:password/:email", async function(req, res) { 
+    // returns false if user already exists, true if created successfully
+    
+    const newUser = new User({
+        username: req.params.username,
+        password: req.params.password,
+        email: req.params.email
+    })
+    
+    try {
+        let foundUser = await User.findOne({username: req.params.username})
+        if (foundUser == null) {
+            try {
+                await newUser.save()
+                res.send(true)
+            } catch (error) {
+                res.send(error)
+            }
+        }
+        else {
+            res.send(false)
+        }
+        
+    }
+    catch (error) {
+        res.send(error)
+    }
+
+})
+
+app.post("/check-password/:username/:password", async function(req, res) { 
+    // sends true if password matches for the user, false if user doesn't exist or password doesn't match
+
+    const searchUsername = req.params.username
+    const password = req.params.password
+
+    try {
+        let foundUser = await User.findOne({username: searchUsername})
+        if (foundUser == null) {
+            // if user doesn't exist, return false
+            res.send(false)
+        }
+        else {
+            // return true if passwords match, false otherwise
+            res.send(foundUser.password == password)
+        }
+    }
+    catch (error) {
+        res.send(error)
+    }
+    
+})
+
+
 app.listen(8000, function(req, res) {
     console.log("Listening on port 8000")
 })
