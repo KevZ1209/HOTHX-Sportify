872852bbc6245bca6ceffaa9508128643f99ff07
Charles Bucquet <charles@bucquet.com>
2023-03-05 11:17:34 -0800

    Implemented routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,41 @@
 import React from "react";
+import { BrowserRouter,Routes, Route, NavLink } from 'react-router-dom';
+import HomePage from "./HomePage";
+import ProfilePage from "./ProfilePage";
+import LeaderboardPage from "./LeaderboardPage";
 
 function App() {
 	return (
-        <div>
-          <h1>Hi there!</h1>
-        </div>
-      );
+    <BrowserRouter>
+      <NavLink
+        to="/"
+        style={{ textDecoration: "none", marginRight: "0.5rem" }}
+        disableRipple
+      >
+        <div>HOME</div>
+      </NavLink>
+      <NavLink
+        to="/profile"
+        style={{ textDecoration: "none", marginRight: "0.5rem" }}
+        disableRipple
+      >
+        <div>PROFILE</div>
+      </NavLink>
+      <NavLink
+        to="/leaderboard"
+        style={{ textDecoration: "none", marginRight: "0.5rem" }}
+        disableRipple
+      >
+        <div>LEADERBOARD</div>
+      </NavLink>
+      <Routes>
+        <Route exact path="/" element={<HomePage/>} />
+        <Route path="/profile" element={<ProfilePage/>} />
+        <Route path="/leaderboard" element={<LeaderboardPage/>} />
+      </Routes>
+    </BrowserRouter>
+
+  );
 }
 
 export default App;
---
+++ client/src/ProfilePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProfilePage() {
+    return (
+    <div>
+        <h1>Profile page!</h1>
+    </div>
+    );
+}
+
+export default ProfilePage;
